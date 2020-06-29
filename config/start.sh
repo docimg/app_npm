047453ee9f9c8962b25f3446a512fd3c45f6f29f
GITEE <gitee@wujiaxing.cn>
2020-06-29 23:32:30 +0800

    update

--- config/start.sh
+++ config/start.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+if [ ! -d "/var/lib/mysql/mysql" ];then
+    mysql_install_db --defaults-file=/etc/mysql/my.cnf --user=root > /dev/null
+fi
+
 /usr/bin/mysqld --defaults-file=/etc/mysql/my.cnf --user=root --console --character-set-server=utf8 &
 
-mysql -uroot -e "source /tmp/privilege.sql;"
+if [ ! -d "/var/lib/mysql/mysql" ];then
+    mysql -uroot -e "source /tmp/privilege.sql;"
+fi
 
 php-fpm &
 
