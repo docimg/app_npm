673362dabeeac52361233f60d9c7a8bf60e18f12
GITEE <gitee@wujiaxing.cn>
2020-06-30 10:14:35 +0800

    update

--- config/start.sh
+++ config/start.sh
@@ -2,21 +2,24 @@
 
 if [ ! -d "/var/lib/mysql/mysql" ];then
     echo "[i] mysql_install_db"
-    /usr/bin/mysql_install_db --user=root > /dev/null
+    mysql_install_db --user=root > /dev/null
+
     echo "[i] start mysql service"
+    mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=root --character-set-server=utf8 &
     mysql_ready() {
-        /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=root --character-set-server=utf8 &
+        mysqladmin ping --socket=/etc/mysql/mysql.sock --user=root > /dev/null 2>&1
     }
     while !(mysql_ready)
     do
-        echo "waiting for mysql ..."
+        echo "[i] waiting for mysql ..."
         sleep 3
     done
+    
     echo "[i] init mysql privilege"
-    /usr/bin/mysql -uroot -e "source /tmp/privilege.sql;"
+    mysql -uroot -e "source /tmp/privilege.sql;"
 else
     echo "[i] start mysql service"
-    /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=root --character-set-server=utf8 &
+    mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=root --character-set-server=utf8 &
 fi
 
 php-fpm &
