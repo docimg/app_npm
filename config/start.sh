176aabc1388a1228acc56e620087d8176cde1089
GITEE <gitee@wujiaxing.cn>
2020-06-30 09:50:15 +0800

    update

--- config/start.sh
+++ config/start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 if [ ! -d "/var/lib/mysql/mysql" ];then
+    echo "[i] mysql_install_db"
     /usr/bin/mysql_install_db --user=root > /dev/null
 fi
 
 /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=root --character-set-server=utf8 &
 
 if [ ! -d "/var/lib/mysql/mysql" ];then
+    echo "[i] init mysql privilege"
     /usr/bin/mysql -uroot -e "source /tmp/privilege.sql;"
 fi
 
