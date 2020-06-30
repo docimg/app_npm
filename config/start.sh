d3cee6b588e09db001ba9e875a4b359bd3070050
GITEE <gitee@wujiaxing.cn>
2020-06-30 09:54:50 +0800

    update

--- config/start.sh
+++ config/start.sh
@@ -3,17 +3,17 @@
 if [ ! -d "/var/lib/mysql/mysql" ];then
     echo "[i] mysql_install_db"
     /usr/bin/mysql_install_db --user=root > /dev/null
-fi
-
-/usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=root --character-set-server=utf8 &
-
-if [ ! -d "/var/lib/mysql/mysql" ];then
+    echo "[i] start mysql service"
+    /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=root --character-set-server=utf8 &
     echo "[i] init mysql privilege"
     /usr/bin/mysql -uroot -e "source /tmp/privilege.sql;"
+else
+    echo "[i] start mysql service"
+    /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf --user=root --character-set-server=utf8 &
 fi
 
 php-fpm &
 
 nginx -g "daemon off;" &
 
-# rm -rf /tmp/*
+rm -rf /tmp/*
